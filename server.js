fc7b7ac06853e796f349368b115c27c4e023fc5a
cluper33-gif <c.luper33@gmail.com>
2026-04-04 20:27:55 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Backend running 💪");
+});
+
+app.listen(10000, () => {
+    console.log("Server started");
+});
